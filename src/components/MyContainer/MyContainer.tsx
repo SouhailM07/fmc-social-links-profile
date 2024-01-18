dd43cdde4e3b0ee9977cc3168db62c10d45d9c7b
shadow <lightshadow416@gmail.com>
2024-01-18 21:04:48 +0100

    project core was built

---
+++ src/components/MyContainer/MyContainer.tsx
@@ -0,0 +1,5 @@
+import "./mycontainer.css";
+// components
+export default function MyContainer() {
+  return <>MyContainer</>;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import(tailwindcss).Config} */ export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        Grey: "#333333",
+        DarkGrey: "#1F1F1F",
+        OffBlack: "#121212",
+      },
+    },
+  },
+  plugins: [],
+};
